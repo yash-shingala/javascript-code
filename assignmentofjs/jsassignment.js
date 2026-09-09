5e3bde0a48553936a2a0a2b86cf1d30548b0f7f8
yash-shingala <yash.shingala.cg@gmail.com>
2026-09-09 18:43:05 +0530

    js assignment added

---
+++ assignmentofjs/jsassignment.js
@@ -0,0 +1,18 @@
+// 10
+// let value = 25;
+// console.log(typeof value);
+
+// value = "JavaScript";
+// console.log(typeof value);
+
+// value = false;
+// console.log(typeof value);
+
+// number
+// string
+// boolean
+
+
+
+
+
---
+++ firstjs/index.js
@@ -0,0 +1,58 @@
+// console.log("Hello World");
+// let a = 10+5;
+// console.log(a);
+
+// let FirstName="raj";
+// console.log(FirstName);
+
+// console.log(typeof(42));
+// console.log(typeof 42);    
+
+
+// function division(){
+//     var p=20;
+//     var q=10;
+//     return p/q;
+// };
+
+// console.log(division());
+
+
+
+
+// function division(p,q){
+//     return p/q;
+// };
+
+// console.log(division(30,10));
+
+
+
+// function demo(){
+//     console.log("hello");
+//     console.log(3);
+// }
+
+// demo();
+
+
+
+// create a function having name calculate which have 2 parameter a,b and print addition subtraction multiplication division of given parametres
+// function calculate(a,b){
+//     console.log(a+b);
+//     console.log(a-b);
+//     console.log(a*b);
+//     console.log(a/b);
+// }
+
+// calculate(2,4);
+
+
+// function greet(){
+//     return "hello everyone";
+// };
+
+// console.log(greet());
+
+
+
